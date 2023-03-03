a1b6262e42e7cb43a822bf53bb9cbe0a76ffbff4
Nicholas Grimaldi <98732607+nickyg-42@users.noreply.github.com>
2023-03-02 20:50:10 -0500

    initial commit

---
+++ public/main.js
@@ -0,0 +1,134 @@
+const logout = document.getElementById('logout');
+const dropdown = document.getElementById('devices');
+const apply = document.getElementById('apply');
+const warn = document.getElementById('warn');
+const playlistsDiv = document.getElementById('ps');
+var playlists = [];
+var owner = "";
+var deviceId = "";
+
+apply.addEventListener('click', () => {
+    var selection = dropdown.value;
+
+    if (selection === "none") {
+        warn.style.display = "block";
+        playlistsDiv.style.display = "none";
+    }
+    else {
+        warn.style.display = "none";
+        playlistsDiv.style.display = "block";
+
+        deviceId = selection;
+    }
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('http://localhost:8383/devices', {
+        method: 'GET'
+    })
+    .then((response) => response.json())
+    .then(data => {
+        for (let l = 0; l < data.devices.length; l++) {
+            var curr = data.devices[l];
+            var opt = document.createElement("option");
+            opt.text = curr.name + " - " + curr.type;
+            opt.value = curr.id;
+            dropdown.append(opt);
+        }
+    })
+    .catch(error => alert(error));
+
+    fetch('http://localhost:8383/user', {
+        method: 'GET'
+    })
+    .then((response) => response.json())
+    .then(data => {
+        owner = data.id;
+    })
+    .catch(err => alert(err));
+
+    fetch('http://localhost:8383/playlists', {
+        method: 'GET'
+    })
+    .then((response) => response.json())
+    .then(data => {
+        for (let i = 0; i < data.items.length; i++) {
+            var curr = data.items[i];
+
+            if (curr.owner.id === owner) playlists.push({ 
+                trackCount: curr.tracks.total, 
+                image: curr.images[0] ? curr.images[0].url : null,
+                id: curr.id, 
+                name: curr.name 
+            });
+        }
+
+        var container = document.getElementById('ps');
+
+        playlists.forEach((x) => {
+            var playlistDiv = document.createElement("div");
+            
+            var imgDiv = document.createElement("div");
+            var titleDiv = document.createElement("div");
+            var shuffleDiv = document.createElement("div");
+            var trackCountDiv = document.createElement("div");
+
+            var img = document.createElement("img");
+            var shBtn = document.createElement("button");
+
+            playlistDiv.className = "playlist";
+
+            imgDiv.className = "playlist__art"
+
+            img.src = x.image;
+            img.width = "100";
+            img.height = "100";
+            img.alt = "playlist image"
+
+            titleDiv.className = "playlist__title";
+            titleDiv.innerHTML = x.name;
+
+            shuffleDiv.className = "playlist__shuffle";
+            shBtn.textContent = "Shuffle";
+            shBtn.id = x.id;
+            shBtn.className = "shuffle";
+            shBtn.name = x.name;
+
+            trackCountDiv.className = "playlist__length";
+            trackCountDiv.innerHTML = "Tracks: " + x.trackCount;
+
+            imgDiv.append(img);
+            shuffleDiv.append(shBtn);
+
+            playlistDiv.append(imgDiv);
+            playlistDiv.append(titleDiv);
+            playlistDiv.append(shuffleDiv);
+            playlistDiv.append(trackCountDiv);
+
+            container.append(playlistDiv);
+            container.append(document.createElement("br"));
+        })
+
+        var buttons = document.getElementsByClassName("shuffle");
+
+        for (var k = 0; k < buttons.length; k++) {
+            buttons[k].addEventListener('click', (e) => {
+                fetch(`http://localhost:8383/shuffle/${owner}/${e.target.id}/${e.target.name}/${deviceId}`, {
+                    method: 'GET'
+                })
+                .then((response) => response.json())
+                .then(data => {
+                    console.log('about to fetch: ', data.tempId);
+                    fetch(`http://localhost:8383/delete/${data.tempId}`, {
+                        method: 'GET',
+                    })
+                });
+            })
+        }
+    })
+    .catch(err => alert(err));
+});
+
+logout.addEventListener('click', () => {
+    window.location.href = "index.html";
+});
---
+++ server.js
@@ -0,0 +1,301 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8383;
+const querystring = require('node:querystring');
+const axios = require("axios");
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/delete/:playlistToDelete', function(req, res) {
+  var playlistId = req.params.playlistToDelete;
+
+  axios({
+    method: 'delete',
+    url: `https://api.spotify.com/v1/playlists/${playlistId}/followers`,
+    headers: {
+      'content-type': 'application/json',
+      Authorization: process.env.ACCESS_TOKEN,
+    },
+  })
+  .catch(error => alert(error));
+});
+
+app.get('/devices', function(req, res) {
+  axios({
+    method: 'get',
+    url: 'https://api.spotify.com/v1/me/player/devices',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: process.env.ACCESS_TOKEN,
+    },
+  })
+  .then(response => {
+    if (response.status === 200) {
+      res.send(JSON.stringify(response.data));
+    }
+  })
+  .catch(err => alert(err));
+});
+
+app.get('/shuffle/:userId/:playlistId/:playlistName/:deviceId', function(req, res) {
+  var plId = req.params.playlistId;
+  var userId = req.params.userId;
+  var playlistName = req.params.playlistName;
+  var deviceId = req.params.deviceId;
+  var tracks = [];
+
+  //create dummy playlist
+  axios({
+    method: 'post',
+    url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+    data: {
+      name: "Copy of " + playlistName,
+      description: "temporary copy of " + playlistName,
+      public: "false",
+    },
+    headers: {
+      'content-type': 'application/json',
+      Authorization: process.env.ACCESS_TOKEN,
+    },
+  })
+  .then(response => {
+    if (response.status === 201) {
+      // get newly created playlist id
+      var tempPlId = response.data.id;
+
+      // get tracks from user selected playlist
+      axios({
+        method: 'get',
+        url: `https://api.spotify.com/v1/playlists/${plId}/tracks?fields=total,items%28track.id%2C%20track.name%29`,
+        headers: {
+          'content-type': 'application/json',
+          Authorization: process.env.ACCESS_TOKEN,
+        },
+      })
+      .then(response => {
+        if (response.status === 200) {
+          //add first 100
+          var total = response.data.total;
+          var trackIds = response.data.items;
+          var offset = 100;
+
+          var copyTotal = total - 100;
+
+          for (var i = 0; i < trackIds.length; i++) {
+            tracks.push({id: "spotify:track:" + trackIds[i].track.id, name: trackIds[i].track.name });
+          }
+          
+          var num = 0;
+
+          if (copyTotal <= 0) {
+            num = 0;
+          }
+          else {
+            while (copyTotal > 0) {
+              copyTotal -= 100;
+              num++;
+            }
+          }
+
+          let promises = [];
+
+          // if more than 100, keep getting tracks until done
+          for (var k = 0; k < num; k++) {
+            promises.push(
+              axios({
+                method: 'get',
+                url: `https://api.spotify.com/v1/playlists/${plId}/tracks?offset=${offset}&fields=total,items%28track.id%2C%20track.name%29`,
+                headers: {
+                  'content-type': 'application/json',
+                  Authorization: process.env.ACCESS_TOKEN,
+                },
+              })
+              .then(response => {
+                if (response.status === 200) {
+                  for (var j = 0; j < response.data.items.length; j++) {
+                    tracks.push({id: "spotify:track:" + response.data.items[j].track.id, name: response.data.items[j].track.name });
+                  }
+                }
+              })
+            );
+
+            offset += 100;
+          }
+
+          Promise.all(promises).then(() => {
+            // tracks have been acquired. now randomize them and put them in temp
+            shuffle(tracks);
+            
+            let tracksBatch = [];
+            let batchPromises = [];
+
+            while (tracks.length > 0) {
+              for (let l = 0; l < 100; l++) {
+                if (tracks.length === 0) break;
+                tracksBatch.push(tracks.shift());
+              }
+
+              batchPromises.push(
+                axios({
+                  method: 'post',
+                  url: `https://api.spotify.com/v1/playlists/${tempPlId}/tracks`,
+                  data: {
+                    "uris": tracksBatch.map(tr => tr.id),
+                  },
+                  headers: {
+                    'content-type': 'application/json',
+                    Authorization: process.env.ACCESS_TOKEN,
+                  },
+                })
+                .catch(error => alert(error))
+              );
+
+              tracksBatch = [];
+            }
+
+            Promise.all(batchPromises).then(() => {
+              //toggle shuffle
+              axios({
+                method: 'put',
+                url: `https://api.spotify.com/v1/me/player/shuffle?state=false&device_id=${deviceId}`,
+                headers: {
+                  'content-type': 'application/json',
+                  Authorization: process.env.ACCESS_TOKEN,
+                },
+              })
+              .catch(error => alert(error))
+              .then(() => {
+                //play temp playlist 
+                axios({
+                  method: 'put',
+                  url: `https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`,
+                  data: {
+                    context_uri: `spotify:playlist:${tempPlId}`,
+                  },
+                  headers: {
+                    'content-type': 'application/json',
+                    Authorization: process.env.ACCESS_TOKEN,
+                  },
+                })
+                .catch(error => alert(error));
+
+                //send playlist id back to frontend
+                res.send({tempId: tempPlId});
+              });
+            });
+          });
+        }
+      });
+    }
+  });
+});
+
+app.get('/playlists', function(req, res) {
+  axios({
+    method: 'get',
+    url: 'https://api.spotify.com/v1/me/playlists?limit=50',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: process.env.ACCESS_TOKEN,
+    },
+  })
+  .then(response => {
+    if (response.status === 200) {
+      res.send(JSON.stringify(response.data));
+    }
+  })
+  .catch(err => alert(err));
+});
+
+app.get('/user', function(req, res) {
+  axios({
+    method: 'get',
+    url: 'https://api.spotify.com/v1/me',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: process.env.ACCESS_TOKEN,
+    },
+  })
+  .then(response => {
+    if (response.status === 200) {
+      res.send(JSON.stringify(response.data));
+    }
+  })
+  .catch(err => alert(err));
+});
+
+app.get('/home', function(req, res) {
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+
+  axios({
+    method: 'post',
+    url: 'https://accounts.spotify.com/api/token',
+    data: querystring.stringify({
+      grant_type: 'authorization_code',
+      code: code,
+      state: state,
+      redirect_uri: process.env.REDIRECT_URI,
+    }),
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${new Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
+    },
+  })
+    .then(response => {
+      if (response.status === 200) {
+        const { access_token, token_type } = response.data;
+
+        process.env['ACCESS_TOKEN'] = token_type + ' ' + access_token;
+
+        res.redirect("main.html");
+      } else {
+        res.send(response);
+      }
+    })
+    .catch(error => alert(error));
+});
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  var scope = "user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-currently-playing app-remote-control playlist-read-private playlist-modify-private playlist-modify-public user-read-playback-position user-top-read user-read-recently-played";
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: process.env.CLIENT_ID,
+      scope: scope,
+      redirect_uri: process.env.REDIRECT_URI,
+      state: state,
+      show_dialog: "true",
+    }));
+});
+
+app.listen(port, () => {
+    console.log(`Server has started on port: ${port}`);
+})
+
+const generateRandomString = (myLength) => {
+  const chars =
+    "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890";
+  const randomArray = Array.from(
+    { length: myLength },
+    (v, k) => chars[Math.floor(Math.random() * chars.length)]
+  );
+
+  const randomString = randomArray.join("");
+  return randomString;
+};
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+  }
+}
